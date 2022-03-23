b605880e91a739096617aba55152aab460b45b31
kentny <kentaro.terasaki@gmail.com>
2022-03-24 07:53:01 +0900

    Add simple solidity smart contract code.

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.1;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("Hello, this is my NFT contract!");
+    }
+}
