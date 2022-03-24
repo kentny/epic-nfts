1bea2c35f2dc3e9ff198b3ba7e6931eb12fc886f
kentny <kentaro.terasaki@gmail.com>
2022-03-24 18:21:04 +0900

    Add code to deploy sample NFT asset json url.

--- scripts/run.js
+++ scripts/run.js
@@ -3,6 +3,14 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to: ", nftContract.address);
+
+    let txn = await nftContract.makeAnEpicNFT();
+    // Wait for it to be mined.
+    await txn.wait();
+
+    txn = await nftContract.makeAnEpicNFT();
+    // Wait for it to be mined.
+    await txn.wait();
 };
 
 const runMain = async () => {
