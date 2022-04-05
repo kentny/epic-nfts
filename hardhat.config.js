655f8019c735e9fff23725ebd53e329554308000
kentny <kentaro.terasaki@gmail.com>
2022-04-06 07:31:22 +0900

    Add Etherscan tool.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,16 @@
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -27,6 +29,9 @@ module.exports = {
         chainId: 1,
         url: process.env.PROD_ALCHEMY_KEY,
         accounts: [process.env.PRIVATE_KEY],
-    }
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
